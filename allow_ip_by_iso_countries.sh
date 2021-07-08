154709e4324cad22975ba7f33830ef88828c45bb
leitz <leitzner.aguey@gmail.com>
2021-07-08 18:18:10 -0500

    fix and adding script to allow ISO countries list of ip

--- allow_ip_by_iso_countries.sh
+++ allow_ip_by_iso_countries.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Purpose: Block all traffic from specific countries. Use ISO code, example ca for canada. #
-# the script uses iptables and by default block any traffic from the clients
+# Purpose: White list all traffic from specific countries. Use ISO code, example ec for Ecuador. #
+# the script uses iptables and by default Allow any traffic from the clients
 # -------------------------------------------------------------------------------
-COUNTRYISOS="ca" 
+COUNTRYISOS="ec" 
  
 ### Set PATH ###
 IPT=/sbin/iptables
@@ -16,8 +16,11 @@ LISTISOSOURCE="http://www.ipdeny.com/ipblocks/data/countries"
 # create a dir if not exists
 [ ! -d $ZONEROOT ] && /bin/mkdir -p $ZONEROOT
 
-# clean ip tables rules
+# clean ip tables rules and accept connections
 $IPT -F
+$IPT -P INPUT ACCEPT
+$IPT -P FORWARD ACCEPT
+$IPT -P OUTPUT ACCEPT
   
 for c  in $COUNTRYISOS
 do 
@@ -28,18 +31,25 @@ do
 	$WGET -O $tDB $LISTISOSOURCE/$c.zone
  
 	# get list of ips for that ISO country from local directory
-	BLACKLISTIP=$(egrep -v "^#|^$" $tDB)
-	for iptoblock in $BLACKLISTIP
+	WHITELISTIP=$(egrep -v "^#|^$" $tDB)
+	for iptoallow in $WHITELISTIP
 	do
 	   # block any type of traffic from the listed block of ips
-       $IPT -A INPUT -s $iptoblock -j DROP
-
+       $IPT -A INPUT -s $iptoallow -j ACCEPT
+       
 	   # to block specific port, for example http 80
-	   #$IPT -A INPUT -s $iptoblock -p tcp --destination-port 80 -j DROP
+	   #$IPT -A INPUT -s $iptoallow -p tcp --destination-port 80 -j ACCEPT
 
 	   # comment this line if you dont want to see what ip is being blocked in the terminal.
-       echo $iptoblock
+       echo $iptoallow
 	done
 done
+
+
+# block all the rest of traffic and allow connections from de server
+$IPT -I INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+$IPT -P INPUT DROP
+$IPT -P FORWARD DROP
+$IPT -P OUTPUT ACCEPT
 
 exit 0
